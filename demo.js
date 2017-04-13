3eaf78a11226aff03b61615b5ecd24bc2efecdfc
Kazu Kohakura <kazuaki.kohakura@canadadrives.ca>
2017-04-13 09:35:02 -0700

    fix jshint errors

--- demo.js
+++ demo.js
@@ -1,9 +1,11 @@
-var http = require('http')
+/*jshint esversion: 6 */
+
+var http = require('http');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('travis demo')
-}).listen(8080, '127.0.0.1')
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('travis demo');
+}).listen(8080, '127.0.0.1');
 
-console.log('server running at http://127.0.0.1:8080')
+console.log('server running at http://127.0.0.1:8080');
 
